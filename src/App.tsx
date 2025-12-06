e72d14ef6509e8294eb0bd2acf4404aaef242e38
faisaliqbalpial <248058976+faisaliqbalpial@users.noreply.github.com>
2025-12-06 13:49:08 +0600

    WP Button

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,8 @@ import Portfolio from "./pages/Portfolio";
 import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
 
+import { WhatsAppButton } from "./components/WhatsAppButton";
+
 const queryClient = new QueryClient();
 
 const App = () => (
@@ -29,6 +31,7 @@ const App = () => (
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
+        <WhatsAppButton />
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
